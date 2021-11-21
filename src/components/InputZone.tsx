86523db21556741a500e4e1a65804a916d195eef
田き <prgrm2960.17chan@gmail.com>
2021-11-21 13:48:31 +0900

    ボタンゾーンを作ってみた

---
+++ src/components/InputZone.tsx
@@ -0,0 +1,15 @@
+export function InputZone() {
+  return (
+    <>
+      <input
+        type="text"
+        className="name-input-button"
+        autoComplete="on"
+        placeholder="概念を入力"
+      ></input>
+      <button type="button" className="name-delete-button">
+        <span>削除</span>
+      </button>
+    </>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { InputZone } from "./components/InputZone";
 
 //rootタグのついた場所にReactでつくったものを入れるのだ
 ReactDOM.render(
-  <StrictMode>Hello React TypeScript!!</StrictMode>,
+  <StrictMode>
+    <InputZone />
+    <InputZone />
+    <InputZone />
+  </StrictMode>,
   document.getElementById("root")
 );

3aab1007e6f3c62179d2d6a163de57493648e4c9
田き <prgrm2960.17chan@gmail.com>
2021-11-24 16:37:23 +0900

    useReduceを使ってマージソート状態を管理できるようになったと思う

--- src/components/GotoSortButton.tsx
+++ src/components/GotoSortButton.tsx
@@ -4,11 +4,7 @@ type Props = {
 
 export function GotoSortButton(props: Props) {
   return (
-    <button
-      type="button"
-      className="add-item-button"
-      onClick={() => props.pushEvent()}
-    >
+    <button type="button" className="add-item-button" onClick={props.pushEvent}>
       <span>ソートする！</span>
     </button>
   );
